84d2f46de4eba98f391af7f9edb5f71b37adf177
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-11 04:20:11 -0300

    feat: better supplier form

--- src/components/SupplierForm.vue
+++ src/components/SupplierForm.vue
@@ -39,98 +39,107 @@ const onSubmit = handleSubmit(async (values) => {
 </script>
 
 <template>
-  <form class="w-full space-y-6 p-10" @submit="onSubmit">
-    <FormField v-slot="{ componentField }" name="document">
-      <FormItem v-auto-animate>
-        <FormLabel>CNPJ</FormLabel>
-        <FormControl>
-          <Input :mask="'##.###.###/000#-##'" type="text" v-bind="componentField" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
+  <form class="w-full" @submit="onSubmit">
+    <div class="mb-5 flex flex-row justify-around">
+      <div>
+        <span>Fornecedor</span>
+        <Separator class="mb-2" />
+        <FormField v-slot="{ componentField }" name="document">
+          <FormItem v-auto-animate>
+            <FormLabel>CNPJ</FormLabel>
+            <FormControl>
+              <Input :mask="'##.###.###/000#-##'" type="text" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
 
-    <FormField v-slot="{ componentField }" name="business_name">
-      <FormItem v-auto-animate>
-        <FormLabel>Nome</FormLabel>
-        <FormControl>
-          <Input type="text" v-bind="componentField" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
+        <FormField v-slot="{ componentField }" name="business_name">
+          <FormItem v-auto-animate>
+            <FormLabel>Nome</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+      </div>
+      <div>
+        <span>Endereço</span>
+        <Separator class="mb-2" />
+        <FormField v-slot="{ componentField }" name="address.zipcode">
+          <FormItem v-auto-animate>
+            <FormLabel>CEP</FormLabel>
+            <FormControl>
+              <Input mask="#####-###" type="text" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
 
-    <FormField v-slot="{ componentField }" name="address.zipcode">
-      <FormItem v-auto-animate>
-        <FormLabel>CEP</FormLabel>
-        <FormControl>
-          <Input mask="#####-###" type="text" v-bind="componentField" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
+        <FormField v-slot="{ componentField }" name="address.city">
+          <FormItem v-auto-animate>
+            <FormLabel>Cidade</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
 
-    <FormField v-slot="{ componentField }" name="address.city">
-      <FormItem v-auto-animate>
-        <FormLabel>Cidade</FormLabel>
-        <FormControl>
-          <Input type="text" v-bind="componentField" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
+        <FormField v-slot="{ componentField }" name="address.state">
+          <FormItem v-auto-animate>
+            <FormLabel>Estado</FormLabel>
+            <FormControl>
+              <Select v-bind="componentField">
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um Estado" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Estado</SelectLabel>
+                    <SelectItem v-for="state of StatesEnum" :key="state" :value="state">
+                      {{ state }}
+                    </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
 
-    <FormField v-slot="{ componentField }" name="address.state">
-      <FormItem v-auto-animate>
-        <FormLabel>Estado</FormLabel>
-        <FormControl>
-          <Select v-bind="componentField">
-            <SelectTrigger>
-              <SelectValue placeholder="Selecione um Estado" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Estado</SelectLabel>
-                <SelectItem v-for="state of StatesEnum" :key="state" :value="state">
-                  {{ state }}
-                </SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
+        <FormField v-slot="{ componentField }" name="address.street">
+          <FormItem v-auto-animate>
+            <FormLabel>Rua</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
 
-    <FormField v-slot="{ componentField }" name="address.street">
-      <FormItem v-auto-animate>
-        <FormLabel>Rua</FormLabel>
-        <FormControl>
-          <Input type="text" v-bind="componentField" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
+        <FormField v-slot="{ componentField }" name="address.number">
+          <FormItem v-auto-animate>
+            <FormLabel>Número</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
 
-    <FormField v-slot="{ componentField }" name="address.number">
-      <FormItem v-auto-animate>
-        <FormLabel>Número</FormLabel>
-        <FormControl>
-          <Input type="text" v-bind="componentField" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
-
-    <FormField v-slot="{ componentField }" name="address.complement">
-      <FormItem v-auto-animate>
-        <FormLabel>Complemento</FormLabel>
-        <FormControl>
-          <Input type="text" v-bind="componentField" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
+        <FormField v-slot="{ componentField }" name="address.complement">
+          <FormItem v-auto-animate>
+            <FormLabel>Complemento</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+      </div>
+    </div>
     <div class="flex flex-col items-end justify-center">
       <Button type="submit"> Salvar </Button>
     </div>
