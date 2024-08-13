4df8da7aaa2c4224d753bd66b3616a83e179022f
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-12 23:53:03 -0300

    feat: product form

--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -14,7 +14,7 @@ const formSchema = [
   }),
   z.object({
     quantity: z.number(),
-    state: z.nativeEnum(UnitEnum)
+    unit: z.nativeEnum(UnitEnum)
   })
 ]
 
@@ -40,9 +40,90 @@ const steps = [
       v-model:step-index="stepIndex"
       :on-submit="onSubmit"
     >
-      <template v-if="stepIndex === 1"> </template>
+      <template v-if="stepIndex === 1">
+        <FormField v-slot="{ componentField }" name="name">
+          <FormItem v-auto-animate>
+            <FormLabel>Nome do Produto</FormLabel>
+            <FormControl>
+              <Input type="text" v-bind="componentField" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
 
-      <template v-if="stepIndex === 2"> Packaging Form </template>
+        <FormField v-slot="{ componentField }" name="average_unit_weight">
+          <FormItem>
+            <FormLabel>Peso Médio</FormLabel>
+            <NumberField
+              class="gap-2"
+              :min="0"
+              :default-value="0.1"
+              :format-options="{
+                signDisplay: 'exceptZero',
+                minimumFractionDigits: 2
+              }"
+              v-bind="componentField"
+            >
+              <NumberFieldContent>
+                <NumberFieldDecrement />
+                <FormControl>
+                  <NumberFieldInput />
+                </FormControl>
+                <NumberFieldIncrement />
+              </NumberFieldContent>
+            </NumberField>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+      </template>
+
+      <template v-if="stepIndex === 2">
+        <FormField v-slot="{ componentField }" name="packaging.quantity">
+          <FormItem>
+            <FormLabel>Quantidade que cabe na caixa</FormLabel>
+            <NumberField
+              class="gap-2"
+              :min="0"
+              :default-value="0.1"
+              :format-options="{
+                signDisplay: 'exceptZero',
+                minimumFractionDigits: 1
+              }"
+              v-bind="componentField"
+            >
+              <NumberFieldContent>
+                <NumberFieldDecrement />
+                <FormControl>
+                  <NumberFieldInput />
+                </FormControl>
+                <NumberFieldIncrement />
+              </NumberFieldContent>
+            </NumberField>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+        <FormField v-slot="{ componentField }" name="packaging.unit">
+          <FormItem v-auto-animate>
+            <FormLabel>Unidade</FormLabel>
+            <FormControl>
+              <Select v-bind="componentField">
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione a unidade do empacotamento" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Unidade</SelectLabel>
+                    <SelectItem v-for="unit of UnitEnum" :key="unit" :value="unit">
+                      {{ unit }}
+                    </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+      </template>
     </SteppedForm>
   </div>
 </template>
