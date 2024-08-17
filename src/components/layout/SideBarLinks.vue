5f50def380f2e28ce425f1256b550e0050cc25b6
Antonio Bertino de Vasconcelos Cabral Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-17 12:50:38 -0300

    feat: improve nav (#9)

--- src/components/layout/SideBarLinks.vue
+++ src/components/layout/SideBarLinks.vue
@@ -1,24 +1,51 @@
 <script setup lang="ts">
 interface LinkProp {
   title: string
-  to: string
   icon: string
+  to?: string
+  children?: {
+    title: string
+    to: string
+    icon: string
+  }[]
 }
 
 defineProps<{
-  links: LinkProp[]
+  routes: LinkProp[]
 }>()
 </script>
 
 <template>
-  <RouterLink
-    exact-active-class="text-primary bg-muted"
-    v-for="link in links"
-    :key="link.title"
-    :to="link.to"
-    class="mx-2 flex items-center justify-center gap-3 rounded-lg px-4 py-2 text-muted-foreground transition-colors hover:text-primary lg:justify-normal"
-  >
-    <iconify-icon :icon="link.icon"></iconify-icon>
-    <span class="hidden text-nowrap lg:block">{{ link.title }}</span>
-  </RouterLink>
+  <div v-for="route in routes" :key="route.title">
+    <RouterLink
+      v-if="route.to"
+      exact-active-class="text-primary bg-muted"
+      :to="route.to"
+      class="flex items-center justify-center gap-3 px-4 py-2 text-muted-foreground transition-colors hover:text-primary lg:justify-normal"
+    >
+      <iconify-icon :icon="route.icon"></iconify-icon>
+      <span class="hidden text-nowrap lg:block"> {{ route.title }} </span>
+    </RouterLink>
+
+    <div
+      class="flex items-center justify-center gap-3 px-4 py-2 text-muted-foreground transition-colors hover:text-primary lg:justify-normal"
+      v-else
+    >
+      <iconify-icon :icon="route.icon"></iconify-icon>
+      <span class="hidden text-nowrap lg:block"> {{ route.title }} </span>
+    </div>
+
+    <div class="ml-3">
+      <RouterLink
+        v-for="link in route.children"
+        :key="link.title"
+        exact-active-class="text-primary bg-muted"
+        :to="link.to"
+        class="flex items-center justify-center gap-3 px-4 py-2 text-muted-foreground transition-colors hover:text-primary lg:justify-normal"
+      >
+        <iconify-icon :icon="link.icon"></iconify-icon>
+        <span class="hidden text-nowrap lg:block"> {{ link.title }} </span>
+      </RouterLink>
+    </div>
+  </div>
 </template>
