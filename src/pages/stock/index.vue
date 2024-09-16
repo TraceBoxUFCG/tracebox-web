bd5c431d0103aa47f79c23a2ff581d984d1aa484
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-09-15 21:58:22 -0300

    fix: store hot refresh

---
+++ src/pages/stock/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const pageStore = usePageStore()
+pageStore.title = 'Fornecedores'
+
+const supplierStore = useSupplierStore()
+supplierStore.fill()
+</script>
+
+<template>
+  <SupplierDataTable />
+</template>
